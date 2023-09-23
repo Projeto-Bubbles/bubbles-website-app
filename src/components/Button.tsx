5f8ba81e4ef46ccfe943331383ab8e3a29050b36
helloWorldRuan <ruan.rodrigues@sptech.school>
2023-09-23 13:20:37 -0300

    [feature] update repo

---
+++ src/components/Button.tsx
@@ -0,0 +1,21 @@
+import React, { ReactNode } from 'react';
+import { Icon } from 'phosphor-react';
+
+interface ButtonProps {
+  text: string;
+  color: string;
+  icon?: ReactNode; // Permite que você passe um ícone como prop
+}
+
+function Button(props: ButtonProps) {
+  return (
+    <button
+      className={`flex gap-2 ${props.color} items-center rounded-full px-4 py-1 text-gray-700 font-bold uppercase`}
+    >
+      {props.icon && <span>{props.icon}</span>}
+      {props.text}
+    </button>
+  );
+}
+
+export default Button;
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,46 @@
+import Button from './Button';
+import { SignIn, UserCirclePlus } from 'phosphor-react';
+
+function Navbar() {
+  return (
+    <>
+      <nav className="bg-slate-200 flex justify-between py-6 px-8">
+        <div className="w-24">
+          <img src="../src/assets/bubbles-logo-glass.png" alt="logo" />
+        </div>
+        <div className="flex items-center gap-20 w-max">
+          <ul className="flex gap-10">
+            <a href="">
+              <li className="font-medium">Home</li>
+            </a>
+            <a href="">
+              <li className="font-medium">Sobre nós</li>
+            </a>
+            <a href="">
+              <li className="font-medium">Comunidade</li>
+            </a>
+            <a href="">
+              <li className="font-medium">Time</li>
+            </a>
+          </ul>
+
+          <div className="flex gap-4">
+            <Button
+              text="Entrar"
+              color="bg-zinc-300"
+              icon={<SignIn size={16} color="#182b3e" weight="duotone" />}
+            />
+
+            <Button
+              text="Cadastrar"
+              color="bg-blue-200"
+              icon={<UserCirclePlus size={20} color="#182b3e" weight="duotone" />}
+            />
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+}
+
+export default Navbar;
