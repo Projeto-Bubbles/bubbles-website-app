99faed36f0dc35501b5de8be3ec73281f726d415
Gabriel-MRocha <gabriel.rocha@sptech.school>
2023-10-01 15:19:07 -0300

    [feature] add footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import Hero from './components/Hero';
 import Navbar from './components/Navbar';
 import Team from './components/Team';
+import Footer from './components/Footer';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Navbar />
       <Hero />
       <Team />
+      <Footer/>
     </>
   );
 }
