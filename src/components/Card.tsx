7c7e519b9a98b8699ebfefad6eedc577793cadca
V1niciussB <vinicius.bsilva@sptech.school>
2023-10-11 06:57:53 -0300

    Creating Component

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -2,7 +2,7 @@ import { CardProps } from '../interfaces/ComponentsInterfaces';
 
 function Card(props: CardProps) {
   return (
-    <div className="bg-gray-700 text-slate-100 flex flex-col gap-4 justify-start p-6 rounded-md h-[200px] transition-all ease-out duration-500 group hover:bg-slate-800">
+    <div className="bg-gray-700 text-slate-100 flex flex-col gap-4 justify-start p-6 rounded-md h-full transition-all ease-out duration-500 group hover:bg-slate-800">
       {props.icon && (
         <div className="bg-gray-600/50 w-10 h-10 rounded-lg rounded-br-2xl flex justify-center items-center transition-all ease-out duration-500 group-hover:bg-slate-700">
           {props.icon}
