ae5bb000c6f37fa2f3e516a1a0807be54f2165ec
helloWorldRuan <ruan.rodrigues@sptech.school>
2023-10-25 21:22:23 -0300

    [feature] modify post component and and consume api

--- src/components/common/Post/PostHeader.tsx
+++ src/components/common/Post/PostHeader.tsx
@@ -1,28 +1,52 @@
 import { format } from 'date-fns';
+import { DotsThreeOutline } from 'phosphor-react';
+import Popup, { PopupProps } from './../Popup';
+import { useState } from 'react';
 
 interface PostHeaderProps {
   name: string;
   nickname: string;
+  date: Date;
   isNotLogged?: boolean;
 }
 
 function PostHeader({ name, nickname, isNotLogged }: PostHeaderProps) {
   const date: Date = new Date();
+  const [isVisiblePopup, setVisiblePopup] = useState(false);
 
   return (
     <div
       className={`w-full ${
         isNotLogged ? 'text-zinc-300' : 'text-zinc-500'
-      } flex justify-start items-center gap-6`}
+      } flex justify-between items-center relative`}
     >
-      <h4 className="text-lg font-bold">{name}</h4>
+      <div className="w-3/4 flex justify-start gap-4 items-center">
+        <h4 className="w-max bg-red text-lg font-bold">{name}</h4>
 
-      <div className="w-40 flex justify-between item-center">
-        <span>{`@${nickname}`} </span>
-        <em>•</em>
-        <span>{date.getHours()}h</span>
-        <em>•</em>
-        <span>{format(date, 'dd/MM')} </span>
+        <div className="w-48 flex justify-evenly item-center">
+          <span>{`@${nickname}`} </span>
+          <em>•</em>
+          <span>{date.getHours()}h</span>
+          <em>•</em>
+          <span>{format(date, 'dd/MM')} </span>
+        </div>
+      </div>
+
+      <div
+        onClick={() => setVisiblePopup(!isVisiblePopup)}
+        className={`bg-slate-300/50 px-1 rounded-full transition-all duration-300 ease-in-out cursor-pointer hover:bg-slate-300 ${
+          isVisiblePopup ? '-translate-x-20' : 'translate-x-0'
+        }`}
+      >
+        <DotsThreeOutline size={16} color="#f0f4f8" weight="fill" />
+      </div>
+
+      <div
+        className={`transition duration-300 ease-in-out absolute -right-6 -top-6 ${
+          isVisiblePopup ? 'opacity-100 z-50' : 'opacity-0 -z-50'
+        }`}
+      >
+        <Popup onEdit={onEdit} onDelete={onDelete} />
       </div>
     </div>
   );
--- src/components/pages/Feed.tsx
+++ src/components/pages/Feed.tsx
@@ -10,16 +10,29 @@ import {
 } from 'phosphor-react';
 import { Post } from '../common/Post';
 import { PostType } from '../common/Post/PostRoot';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { getPosts } from '../../services/post-services';
 
 function Feed() {
   const navigate = useNavigate();
+
   const [isExpanded, setExpanded] = useState(false);
+
+  const [posts, setPosts] = useState<any>([]);
+
+  useEffect(() => {
+    async function fetchPosts() {
+      const postsData = await getPosts();
+      setPosts(postsData);
+    }
+
+    fetchPosts();
+  }, []);
 
   return (
     <>
       <Navbar />
-      <main className="min-h-screen bg-[url('../../src/assets/bubbles-effect.png')] bg-cover flex justify-start ">
+      <main className="min-h-screen bg-[url('../../src/assets/bubbles-effect.png')] bg-[1200px] flex justify-start ">
         {/* Sidebar */}
         <div
           role="sidebar"
@@ -61,28 +74,32 @@ function Feed() {
         </div>
 
         {/* Feed Content */}
-        <div className="w-2/6 min-h-screen m-auto flex flex-col justify-start items-center pt-28">
-          <Post.Root type={PostType.VIEW}>
-            <Post.Header name="Ruan" nickname="helloWorldRu" />
-            <Post.Content content="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sequi reprehenderit similique, saepe veniam a beatae exercitationem velit fuga, magnam maxime ab quaerat aperiam tempore eos! Ducimus totam commodi eaque quaerat!" />
-            <label
-              className="self-end bg-zinc-300/50 px-2 uppercase rounded-md text-slate-800 text-right text-sm font-semibold cursor-pointer transition-all duration-200 ease-in hover:bg-zinc-400/20"
-              onClick={() => setExpanded(!isExpanded)}
-            >
-              Ver comentários
-            </label>
-            <div
-              className={`bg-slate-100/50  rounded-md transition-all duration-300 ease-in-out overflow-y-scroll flex flex-col justify-start items-center gap-2 ${
-                isExpanded ? 'h-80' : 'opacity-0 h-0 overflow-hidden'
-              } `}
-            >
-              <Post.Comment content="QUe merda heinn" />
-              <Post.Comment content="QUe merda heinn" />
-              <Post.Comment content="QUe merda heinn" />
-              <Post.Comment content="QUe merda heinn" />
-              <Post.Comment content="QUe merda heinn" />
-            </div>
-          </Post.Root>
+        <div className="w-2/6 min-h-screen m-auto flex flex-col justify-start items-center gap-10 pt-28">
+          {posts.map((post: any) => (
+            <Post.Root key={post.id} type={PostType.VIEW}>
+              <Post.Header
+                name={post.author}
+                nickname="helloWorldRu"
+                date={post.dateTime}
+              />
+              <Post.Content content={post.content} />
+              <label
+                className="self-end bg-zinc-300/50 px-2 uppercase rounded-md text-slate-800 text-right text-sm font-semibold cursor-pointer transition-all duration-200 ease-in hover:bg-zinc-400/20"
+                onClick={() => setExpanded(!isExpanded)}
+              >
+                Ver comentários
+              </label>
+              <div
+                className={`bg-slate-100/50  rounded-md transition-all duration-300 ease-in-out overflow-y-scroll flex flex-col justify-start items-center gap-2 ${
+                  isExpanded ? 'h-80' : 'opacity-0 h-0 overflow-hidden'
+                } `}
+              >
+                {post.comments.map((comment: any) => (
+                  <Post.Comment key={comment.id} content={comment.content} />
+                ))}
+              </div>
+            </Post.Root>
+          ))}
         </div>
       </main>
     </>
