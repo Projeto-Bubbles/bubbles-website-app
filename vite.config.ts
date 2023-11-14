6ecc2029e0ff599784de4293f5e8a9e58c5daa64
PauloCezaretto <paulo.alvares@sptech.school>
2023-11-13 21:16:16 -0300

    [refactor]: relocating image paths for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -2,18 +2,18 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 const resourceMap = {
-  memberGabriel: './assets/members/member_gabriel.png',
-  memberLaviny: './assets/members/member_laviny.png',
-  memberPaulo: './assets/members/member_paulo.png',
-  memberRuan: './assets/members/member_ruan.png',
-  memberVinicius: './assets/members/member_vinicius.png',
-  cookingImage: './assets/MoreBubbles/cooking.jpg',
-  cyclistImage: './assets/MoreBubbles/cyclist.jpg',
-  funImage: './assets/MoreBubbles/fun.jpg',
-  groupImage: './assets/MoreBubbles/group.jpg',
-  bubblesLogoGlass: './assets/bubbles-logo-glass.png',
-  bubblesEffect: './assets/bubbles-effect.png',
-  outBubbles: './assets/out-bubbles-image.png',
+  memberGabriel: './src/assets/members/member_gabriel.png',
+  memberLaviny: './src/assets/members/member_laviny.png',
+  memberPaulo: './src/assets/members/member_paulo.png',
+  memberRuan: './src/assets/members/member_ruan.png',
+  memberVinicius: './src/assets/members/member_vinicius.png',
+  cookingImage: './src/assets/MoreBubbles/cooking.jpg',
+  cyclistImage: './src/assets/MoreBubbles/cyclist.jpg',
+  funImage: './src/assets/MoreBubbles/fun.jpg',
+  groupImage: './src/assets/MoreBubbles/group.jpg',
+  bubblesLogoGlass: './src/assets/bubbles-logo-glass.png',
+  bubblesEffect: './src/assets/bubbles-effect.png',
+  outBubbles: './src/assets/out-bubbles-image.png',
 };
 
 // https://vitejs.dev/config/
