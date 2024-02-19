61a86d1a2f7aba5a79fdac975f14c2bf00a67429
helloWorldRuan <ruan.rodrigues@sptech.school>
2024-02-19 19:19:01 -0300

    mock user

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,15 @@
 import { Outlet } from 'react-router-dom';
 
 function App() {
+  const data = {
+    id: 1,
+    name: 'Chico Bueno',
+    username: 'chico@123',
+    email: 'chico@gmail.com',
+  };
+
+  localStorage.setItem('user', JSON.stringify(data));
+
   return (
     <>
       <div className="w-screen h-screen bg-[url('../src/assets/bubbles-effect.png')] bg-cover bg-repeat-y">
