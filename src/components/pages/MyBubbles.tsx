43a2664598b5013439e67fec1e811781ec1a9ba6
helloWorldRuan <ruan.rodrigues@sptech.school>
2024-05-10 20:46:10 -0300

    [feature] implementing SpeechRecognition POC innovation in chat

--- src/components/pages/MyBubbles.tsx
+++ src/components/pages/MyBubbles.tsx
@@ -1,7 +1,9 @@
-import { PaperPlaneRight } from 'phosphor-react';
+import { Microphone, PaperPlaneRight } from 'phosphor-react';
 import { useEffect, useRef, useState } from 'react';
 import { io } from 'socket.io-client';
+import { useSpeechRecognition } from '../../hooks/useSpeechRecognition';
 import { ChatMessage } from '../../interfaces/chat';
+import { getLocalUser } from '../../services/userServices';
 import { Bubble } from '../common/Bubble';
 import { Chat } from '../common/Chat';
 import Input from '../common/Fields/Input';
@@ -10,8 +12,10 @@ export function MyBubbles() {
   const [message, setMessage] = useState('');
   const [chats, setChats] = useState<ChatMessage[]>([]);
   const [bubbleId, setBubbleId] = useState<number | null>(null);
+  const { isRecording, transcript, handleStartRecording, handleStopRecording } =
+    useSpeechRecognition();
 
-  const userId = Math.floor(Math.random() * 100);
+  const user: any = getLocalUser();
 
   const socket = useRef<any>(null);
 
@@ -26,38 +30,36 @@ export function MyBubbles() {
   useEffect(() => {
     if (socket) {
       socket.current.on('receive', handleReceiveMessage);
-      console.log('bora');
 
       return () => {
         socket.current.off('receive', handleReceiveMessage);
-        console.log('fui embora');
       };
     }
   }, [socket]);
 
   const handleReceiveMessage = (chatMessage: ChatMessage) => {
-    console.log('👽 ~ mensagem recebida');
     setChats((prevChats) => [...prevChats, chatMessage]);
   };
 
-  const sendMessageToServer = async () => {
-    await socket.current.emit('sendMessageToBubble', {
-      id: chats.length + 1,
-      bubble: {
-        id: bubbleId,
-        name: 'Futeboula',
-      },
-      user: {
-        id: userId,
-        username: 'helloWorldRu',
-      },
-      message,
-    });
-  };
-
-  const handleSendMessage = () => {
+  const handleSendMessage = async () => {
     if (message.trim() !== '') {
-      sendMessageToServer();
+      const chatMessage: ChatMessage = {
+        id: chats.length + 1,
+        bubble: {
+          id: bubbleId!!,
+          name: 'Futeboula',
+        },
+        user: {
+          id: user.id,
+          username: user.nickname,
+        },
+        message,
+      };
+
+      await socket.current.emit('sendMessageToBubble', chatMessage);
+
+      setChats((prevChats) => [...prevChats, chatMessage]);
+
       setMessage('');
     }
   };
@@ -68,51 +70,77 @@ export function MyBubbles() {
     }
   };
 
+  const handleSetTranscript = () => {
+    if (!isRecording) {
+      return handleStartRecording();
+    }
+
+    handleStopRecording();
+    setMessage(transcript);
+  };
+
   const joinBubbleChat = (bubbleId: number) => {
-    console.log('👽 ~ socket:', socket);
     setBubbleId(bubbleId);
     socket.current.emit('joinBubble', bubbleId);
   };
-
-  useEffect(() => {
-    console.log('👽 ~ chat:', chats);
-  }, [chats]);
 
   return (
     <main className="w-full h-screen bg-slate-100 grid grid-cols-[7%_72%_21%]">
       <Chat.Sidebar joinBubbleChat={joinBubbleChat} />
 
       <div className="bg-[url('../src/assets/bubbles-effect.png')] bg-cover flex flex-col items-center justify-between px-20 py-10">
-        <div className="w-full flex flex-col justify-start gap-6 max-h-[600px] scrollbar-hide overflow-y-auto">
-          {chats.map((chat) =>
-            userId === userId ? (
-              <Chat.SenderMessage key={chat.id} message={chat.message} />
-            ) : (
-              <Chat.RecipientMessage
-                key={chat.id}
-                user={chat.user}
-                message={chat.message}
+        {bubbleId ? (
+          <>
+            <div className="w-full flex flex-col justify-start gap-6 max-h-[600px] scrollbar-hide overflow-y-auto">
+              {chats.map((chat) =>
+                chat.user.id === user.id ? (
+                  <Chat.SenderMessage key={chat.id} message={chat.message} />
+                ) : (
+                  <Chat.RecipientMessage
+                    key={chat.id}
+                    user={chat.user}
+                    message={chat.message}
+                  />
+                )
+              )}
+            </div>
+
+            <div className="w-full flex items-center gap-2">
+              <Input
+                placeholder={`${
+                  isRecording
+                    ? 'Gravando... clique novamente para parar'
+                    : 'Mensagem'
+                }`}
+                disabled={isRecording}
+                color="bg-zinc-200"
+                value={message}
+                onChange={(e) => setMessage(e.target.value)}
+                onKeyDown={(e) => handleKeyDown(e)}
               />
-            )
-          )}
-        </div>
 
-        <div className="w-full flex items-center gap-2">
-          <Input
-            placeholder="Mensagem"
-            color="bg-zinc-200"
-            value={message}
-            onChange={(e) => setMessage(e.target.value)}
-            onKeyDown={(e) => handleKeyDown(e)}
-          />
+              <button
+                onClick={() => handleSetTranscript()}
+                className={`size-10 grid place-content-center rounded-md cursor-pointer transition duration-300 ease-in-out ${
+                  isRecording
+                    ? 'animate-pulse bg-red-500'
+                    : 'bg-green-200 hover:bg-green-300 '
+                }`}
+              >
+                <Microphone size={20} color="#0F172A" weight="duotone" />
+              </button>
 
-          <div
-            onClick={() => handleSendMessage()}
-            className="bg-blue-200 w-[2.5rem] h-[2.5rem] grid place-content-center rounded-md cursor-pointer transition duration-300 ease-in-out hover:bg-blue-300"
-          >
-            <PaperPlaneRight size={20} color="#0F172A" weight="duotone" />
-          </div>
-        </div>
+              <button
+                onClick={() => handleSendMessage()}
+                className="bg-blue-200 size-10 grid place-content-center rounded-md cursor-pointer transition duration-300 ease-in-out hover:bg-blue-300"
+              >
+                <PaperPlaneRight size={20} color="#0F172A" weight="duotone" />
+              </button>
+            </div>
+          </>
+        ) : (
+          <h1>Opa! Comece a conversar com suas bolhas!</h1>
+        )}
       </div>
 
       <Bubble.Details />
