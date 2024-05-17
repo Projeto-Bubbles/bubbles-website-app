7c82699bf3db615ca681832fd005b89bfabc1c38
Paulo-Alvares <paulo.alvares@sptech.school>
2024-05-16 21:15:10 -0300

    [refactor]: fixing the instance IP and correcting route paths on the front-end

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 const token = sessionStorage?.getItem('token')
 
 export const api = axios.create({
-    baseURL: 'http://52.90.233.196/api',
+    baseURL: 'http://34.195.120.16/api',
     headers: {
         Authorization: `Bearer ${token || ''}`
     }
